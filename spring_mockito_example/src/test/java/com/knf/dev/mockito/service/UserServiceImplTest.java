05985d5e17ec6e21388216d07c07402cfe4ff754
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-10-30 13:19:19 +0530

    Update UserServiceImplTest.java

--- spring_mockito_example/src/test/java/com/knf/dev/mockito/service/UserServiceImplTest.java
+++ spring_mockito_example/src/test/java/com/knf/dev/mockito/service/UserServiceImplTest.java
@@ -4,47 +4,36 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import com.knf.dev.mockito.entity.User;
 import com.knf.dev.mockito.repository.UserRepository;
 
 @RunWith(SpringRunner.class)
 public class UserServiceImplTest {
 
-    @TestConfiguration
-    static class UserServiceImplTestContextConfiguration {
+	@InjectMocks
+	private UserServiceImpl userService;
 
-        @Bean
-        public UserService userService() {
-            return new UserServiceImpl();
-        }
-    }
+	@Mock
+	private UserRepository userRepository;
 
-    @Autowired
-    private UserService userService;
+	@Before
+	public void setUp() {
+		User user = new User();
+		user.setName("sibin");
+		user.setId(1);
+		Mockito.when(userRepository.findByName(user.getName())).thenReturn(user);
+	}
 
-    @MockBean
-    private UserRepository userRepository;
+	@Test
+	public void whenValidName_thenEmployeeShouldBeFound() {
+		String name = "sibin";
+		User found = userService.getUserByName(name);
 
-    @Before
-    public void setUp() {
-        User user = new User();
-        user.setName("sibin");
-        user.setId(1);
-        Mockito.when(userRepository.findByName(user.getName())).
-                                 thenReturn(user);
-    }
-
-    @Test
-    public void whenValidName_thenEmployeeShouldBeFound() {
-        String name = "sibin";
-        User found = userService.getUserByName(name);
-
-        Assert.assertEquals(found.getName(), name);
-    }
+		Assert.assertEquals(found.getName(), name);
+	}
 }
